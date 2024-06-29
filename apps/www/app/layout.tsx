aba0ff0a1d113d56a84d9bcd08e919c77e6da135
DhrumitPanchal <dhrumitpanchal789@gmail.com>
2024-06-29 19:09:31 +0530

    added footer

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -4,7 +4,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import Navbar from "@/components/nav/Navbar";
-
+import Footer from "@/components/Footer";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -23,6 +23,7 @@ export default function RootLayout({
                 <GoogleOAuthProvider clientId="252350409319-8k8t2r797e3pb4r005gdak1d6kmajgh7.apps.googleusercontent.com">
                     <Navbar />
                     {children}
+                    <Footer />
                 </GoogleOAuthProvider>
             </body>
         </html>
