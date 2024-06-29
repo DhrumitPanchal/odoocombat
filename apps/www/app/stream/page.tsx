8336efae6a98ffd9d690f6ed84ad79c11a352d4d
anascen <webovein@gmail.com>
2024-06-29 14:54:57 +0530

    fix: stream page

--- apps/www/app/stream/page.tsx
+++ apps/www/app/stream/page.tsx
@@ -1,250 +1,57 @@
 "use client";
 
 import { io } from "socket.io-client";
-import { useRef, useEffect, useState } from "react";
+import { useRef, useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Mic, MicOff, Video, VideoOff } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 function Stream() {
+	const [audioState, setAudioState] = useState<boolean>(true);
+	const videoRef = useRef<HTMLVideoElement>(null);
+
 	const configuration = {
 		iceServers: [
 			{
-				urls: [
-					// "stun:stun1.l.google.com:19302",
-					"stun:stun2.l.google.com:19302",
-				],
+				urls: ["stun:stun2.l.google.com:19302"],
 			},
 		],
 		iceCandidatePoolSize: 10,
 	};
 	const socket = io("http://localhost:8080", { transports: ["websocket"] });
 
-	let pc: any;
-	let localStream: any;
-	let startButton: any;
-	let hangupButton: any;
-	let muteAudButton: any;
-	let remoteVideo: any;
-	let localVideo: any;
-	socket.on("message", (e) => {
-		if (!localStream) {
-			console.log("not ready yet");
-			return;
-		}
-		switch (e.type) {
-			case "offer":
-				handleOffer(e);
-				break;
-			case "answer":
-				handleAnswer(e);
-				break;
-			case "candidate":
-				handleCandidate(e);
-				break;
-			case "ready":
-				// A second tab joined. This tab will initiate a call unless in a call already.
-				if (pc) {
-					console.log("already in call, ignoring");
-					return;
-				}
-				makeCall();
-				break;
-			case "bye":
-				if (pc) {
-					hangup();
-				}
-				break;
-			default:
-				console.log("unhandled", e);
-				break;
-		}
-	});
-
-	async function makeCall() {
-		try {
-			pc = new RTCPeerConnection(configuration);
-			pc.onicecandidate = (e: any) => {
-				const message = {
-					type: "candidate",
-					candidate: null,
-				};
-				if (e.candidate) {
-					message.candidate = e.candidate.candidate;
-					// message.sdpMid = e.candidate.sdpMid;
-					// message.sdpMLineIndex = e.candidate.sdpMLineIndex;
-				}
-				socket.emit("message", message);
-			};
-			pc.ontrack = (e: any) =>
-				(remoteVideo.current.srcObject = e.streams[0]);
-			localStream
-				.getTracks()
-				.forEach((track: any) => pc.addTrack(track, localStream));
-			const offer = await pc.createOffer();
-			socket.emit("message", { type: "offer", sdp: offer.sdp });
-			await pc.setLocalDescription(offer);
-		} catch (e) {
-			console.log(e);
-		}
-	}
-
-	async function handleOffer(offer: any) {
-		if (pc) {
-			console.error("existing peerconnection");
-			return;
-		}
-		try {
-			pc = new RTCPeerConnection(configuration);
-			pc.onicecandidate = (e: any) => {
-				const message = {
-					type: "candidate",
-					candidate: null,
-				};
-				if (e.candidate) {
-					message.candidate = e.candidate.candidate;
-					// message.sdpMid = e.candidate.sdpMid;
-					// message.sdpMLineIndex = e.candidate.sdpMLineIndex;
-				}
-				socket.emit("message", message);
-			};
-			pc.ontrack = (e: any) =>
-				(remoteVideo.current.srcObject = e.streams[0]);
-			localStream
-				.getTracks()
-				.forEach((track: any) => pc.addTrack(track, localStream));
-			await pc.setRemoteDescription(offer);
-
-			const answer = await pc.createAnswer();
-			socket.emit("message", { type: "answer", sdp: answer.sdp });
-			await pc.setLocalDescription(answer);
-		} catch (e) {
-			console.log(e);
-		}
-	}
-
-	async function handleAnswer(answer: any) {
-		if (!pc) {
-			console.error("no peerconnection");
-			return;
-		}
-		try {
-			await pc.setRemoteDescription(answer);
-		} catch (e) {
-			console.log(e);
-		}
-	}
-
-	async function handleCandidate(candidate: any) {
-		try {
-			if (!pc) {
-				console.error("no peerconnection");
-				return;
-			}
-			if (!candidate) {
-				await pc.addIceCandidate(null);
-			} else {
-				await pc.addIceCandidate(candidate);
-			}
-		} catch (e) {
-			console.log(e);
-		}
-	}
-	async function hangup() {
-		if (pc) {
-			pc.close();
-			pc = null;
-		}
-		localStream.getTracks().forEach((track: any) => track.stop());
-		localStream = null;
-		startButton.current.disabled = false;
-		hangupButton.current.disabled = true;
-		muteAudButton.current.disabled = true;
-	}
-
-	startButton = useRef(null);
-	hangupButton = useRef(null);
-	muteAudButton = useRef(null);
-	localVideo = useRef(null);
-	remoteVideo = useRef(null);
-	useEffect(() => {
-		hangupButton.current.disabled = true;
-		muteAudButton.current.disabled = true;
-	});
-	const [audiostate, setAudio] = useState(false);
-
-	const startB = async () => {
-		try {
-			localStream = await navigator.mediaDevices.getUserMedia({
-				video: true,
-				audio: { echoCancellation: true },
-			});
-			localVideo.current.srcObject = localStream;
-		} catch (err) {
-			console.log(err);
-		}
-
-		startButton.current.disabled = true;
-		hangupButton.current.disabled = false;
-		muteAudButton.current.disabled = false;
-
-		socket.emit("message", { type: "ready" });
-	};
-
-	const hangB = async () => {
-		hangup();
-		socket.emit("message", { type: "bye" });
-	};
-
 	function muteAudio() {
-		if (audiostate) {
-			localVideo.current.muted = true;
-			setAudio(false);
-		} else {
-			localVideo.current.muted = false;
-			setAudio(true);
-		}
+		setAudioState(!audioState);
 	}
 
 	return (
 		<>
-			<main className="container  ">
-				<div className="video bg-main">
+			<div className="flex flex-col h-full w-full items-center justify-center">
+				<div className="flex w-[600px] max-w-[90%] m-auto gap-y-4 flex-col items-end justify-center">
 					<video
-						ref={localVideo}
-						className="video-item"
+						ref={videoRef}
+						className={cn("rounded-3xl")}
+						width={10000}
 						autoPlay
-						playsInline
-						src=" "
 					></video>
-					<video
-						ref={remoteVideo}
-						className="video-item"
-						autoPlay
-						playsInline
-						src=" "
-					></video>
-				</div>
 
-				<div className="btn">
-					<Button
-						className="btn-item btn-start"
-						ref={startButton}
-						onClick={startB}
-					>
-						<Video></Video>
-					</Button>
-					<Button ref={hangupButton} onClick={hangB}>
-						<VideoOff />
-					</Button>
-					<Button
-						className="btn-item btn-start"
-						ref={muteAudButton}
-						onClick={muteAudio}
-					>
-						{audiostate ? <Mic /> : <MicOff />}
-					</Button>
+					<div className="flex gap-x-4">
+						<Button>
+							<Video className="mr-2"></Video>Start Streaming
+						</Button>
+						<Button variant={"outline"} className="w-12 px0">
+							<VideoOff />
+						</Button>
+						<Button
+							className="w-12 px0"
+							variant={audioState ? "default" : "secondary"}
+							onClick={muteAudio}
+						>
+							{audioState ? <Mic /> : <MicOff />}
+						</Button>
+					</div>
 				</div>
-			</main>
+			</div>
 		</>
 	);
 }
