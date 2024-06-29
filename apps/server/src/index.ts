4197a822fd72438f4920cde5eeec739fc4eb9c77
anascen <webovein@gmail.com>
2024-06-29 09:51:23 +0530

    feat(server): adding web socket

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,1 +1,59 @@
-console.log("server");
+import { createServer, Server } from "http";
+import app from "./app";
+import WebSocket from "ws";
+
+const server: Server = createServer(app);
+const wss: WebSocket.Server = new WebSocket.Server({ server });
+
+let broadcaster: WebSocket | null = null;
+
+wss.on("connection", (ws: WebSocket) => {
+	ws.on("message", (message: string) => {
+		const data = JSON.parse(message);
+
+		switch (data.type) {
+			case "broadcaster":
+				broadcaster = ws;
+				break;
+			case "watcher":
+				ws.send(JSON.stringify({ type: "watcher" }));
+				break;
+			case "offer":
+				if (broadcaster) {
+					broadcaster.send(
+						JSON.stringify({ type: "offer", offer: data.offer })
+					);
+				}
+				break;
+			case "answer":
+				if (broadcaster) {
+					broadcaster.send(
+						JSON.stringify({ type: "answer", answer: data.answer })
+					);
+				}
+				break;
+			case "candidate":
+				if (broadcaster) {
+					broadcaster.send(
+						JSON.stringify({
+							type: "candidate",
+							candidate: data.candidate,
+						})
+					);
+				}
+				break;
+		}
+	});
+
+	ws.on("close", () => {
+		if (ws === broadcaster) {
+			broadcaster = null;
+		}
+	});
+});
+
+const port: number | any = process.env.PORT || 8080;
+
+server.listen(port, () => {
+	console.log("Server is listening on port", port);
+});
